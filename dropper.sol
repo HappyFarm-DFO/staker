08b962ee8c2b622330590152f36673366f6d6908
maxxflyer <maxxflyer@gmail.com>
2020-09-24 19:50:42 +0200

    Update dropper.sol

--- dropper.sol
+++ dropper.sol
@@ -4,18 +4,27 @@ contract TokenDropper{
     ERC20 public token;
     mapping(address => bool)public rewarded;
     uint public multiplier;
+    address master;
+    address public receiver;
     
-    constructor(address staker_contract,uint multip,address token_address){
+    constructor(address staker_contract,uint multip,address token_address,address destination) public{
         staker=HappyStaker(staker_contract);
         multiplier=multip;
         token=ERC20(token_address);
+        master=msg.sender;
+        receiver=destination;
     }
     
     function Pull_Reward() public{
         require(!rewarded[msg.sender]);
-        require(HappyStaker.paused);
-        require(token.transfer(msg.sender, HappyStaker.stake(msg.sender)*multiplier));
+        require(staker.paused());
+        require(token.transfer(msg.sender, staker.stake(msg.sender)*multiplier));
         rewarded[msg.sender]=true;
+    }
+    
+    function burn()public returns(bool){
+        require(!staker.paused());
+        token.transfer(receiver, token.balanceOf(address(this)));
     }
     
 }
